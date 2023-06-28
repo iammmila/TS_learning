53741b555bbb0249e40f225dae3c144d0034c352
Malahat Ibrahimova <ibramila2002@gmail.com>
2023-06-28 19:33:02 +0400

    passing children

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Message from "./assets/Message";
 import ListGroup from "./components/ListGroup";
 import Properties from "./components/Properties";
+import Alert from "./components/Alert";
 
 function App() {
   //? passing data via props
@@ -9,8 +10,14 @@ function App() {
   const handleSelectItem = (group: string) => {
     console.log(group);
   };
+
   return (
     <div>
+      <Alert>
+        <div>myyyy girls</div>
+        <button>hello</button>
+      </Alert>
+
       <Properties items={items} heading={"it is heading which is props"} />
       <Message />
       <p>salalalalllalalm</p>
