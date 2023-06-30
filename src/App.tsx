d4e548da2718aa9f6427e801fcc755b225d6b6f0
Malahat Ibrahimova <ibramila2002@gmail.com>
2023-06-30 23:18:03 +0400

    4.12. Building expandableText Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import "./App.css";
-import CartExercise from "./components/CartExercise";
-import Game from "./components/Game";
-import Pizza from "./components/Pizza";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
   return (
     <>
-      //exercise 1
-      <Game />
-      <br />
-      //exercise 2
-      <Pizza />
-      <br />
-      //exercise 3
-      <CartExercise />
+      <ExpandableText maxChars={10}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad praesentium
+        quis doloremque mollitia repellendus libero corrupti voluptas iure
+        soluta! Est dolorum perferendis corrupti similique hic repudiandae,
+        consectetur error sapiente dolores vero iure voluptas laboriosam odio ex
+        reprehenderit alias repellat beatae at cupiditate expedita quod placeat
+        nisi. Similique dolorem perferendis inventore ipsum fuga quibusdam
+        dolore ad dignissimos quae ratione facere in, repudiandae eum hic quasi
+        rerum, laboriosam ab autem modi odit quia libero? Autem excepturi
+        distinctio, dolorum cupiditate sint ex architecto ea quam? Omnis unde
+        est neque pariatur. Hic neque veniam quisquam quas nisi. Asperiores id a
+        nisi sint, alias autem!
+      </ExpandableText>
     </>
   );
 }
