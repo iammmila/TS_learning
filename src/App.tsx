b28377bb7eacd0b7e24d1f8d3c832eb79b2f8059
Malahat Ibrahimova <ibramila2002@gmail.com>
2023-06-30 20:30:36 +0400

    first method to change object

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import "./App.css";
-import Message from "./components/Message";
+
 function App() {
+  const [drink, setDrink] = useState({
+    title: "Americano",
+    price: 5,
+  });
+
+  const handleClick = () => {
+    //1.we can change object differently:
+    const newDrink = {
+      title: drink.title,
+      price: 6,
+    };
+    setDrink(newDrink);
+    console.log(newDrink);
+  };
+
   return (
     <>
-      <Message />
+      <hr />
+      {drink.price}
+      <hr />
+      <button onClick={handleClick}>Click Please!</button>
     </>
   );
 }
