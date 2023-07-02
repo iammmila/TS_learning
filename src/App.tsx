83b00b38852b8f5bb5fd8226fd14371d8173fd1a
Malahat Ibrahimova <ibramila2002@gmail.com>
2023-07-02 18:24:15 +0400

    5.10. Building ExpenseList

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import "./App.css";
-import Form6 from "./components/Form6";
+import ExpenseList from "./components/Expense-tracker/ExpenseList";
+import { useState } from "react";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "a", amount: 11, category: "utilities1" },
+    { id: 2, description: "bb", amount: 12, category: "utilities2" },
+    { id: 3, description: "ccc", amount: 13, category: "utilities3" },
+    { id: 4, description: "dddd", amount: 14, category: "utilities4" },
+  ]);
+
+  const handleDelete = (id: number) => {
+    setExpenses(expenses.filter((e) => e.id !== id));
+  };
+
   return (
     <>
-      <Form6 />
+      <ExpenseList expenses={expenses} onDelete={(id) => handleDelete(id)} />
     </>
   );
 }
