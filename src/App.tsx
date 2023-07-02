a88e5c50bef8df531274d72e17ed016aa12503b8
Malahat Ibrahimova <ibramila2002@gmail.com>
2023-07-02 15:18:13 +0400

    5.2. Handling Form Submission

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import "./App.css";
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <>
-      <ExpandableText maxChars={10}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad praesentium
-        quis doloremque mollitia repellendus libero corrupti voluptas iure
-        soluta! Est dolorum perferendis corrupti similique hic repudiandae,
-        consectetur error sapiente dolores vero iure voluptas laboriosam odio ex
-        reprehenderit alias repellat beatae at cupiditate expedita quod placeat
-        nisi. Similique dolorem perferendis inventore ipsum fuga quibusdam
-        dolore ad dignissimos quae ratione facere in, repudiandae eum hic quasi
-        rerum, laboriosam ab autem modi odit quia libero? Autem excepturi
-        distinctio, dolorum cupiditate sint ex architecto ea quam? Omnis unde
-        est neque pariatur. Hic neque veniam quisquam quas nisi. Asperiores id a
-        nisi sint, alias autem!
-      </ExpandableText>
+      <Form />
     </>
   );
 }
---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Form = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("submitted");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input id="name" type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          age
+        </label>
+        <input id="age" type="number" className="form-control" />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
