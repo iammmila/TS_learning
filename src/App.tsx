c450fd0169b131b2d33d088cf020c112d5c1aa27
Malahat Ibrahimova <ibramila2002@gmail.com>
2023-07-02 23:26:41 +0400

    6.4. Fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-import ProductList from "./components/ProductList";
+import axios from "axios";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
-  const [category, setCategory] = useState("");
-  //afteRender
+  const [users, setUsers] = useState<User[]>([]);
+
   useEffect(() => {
-    //side effect
-  });
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((response) => setUsers(response.data));
+  }, []);
 
   return (
-    <>
-      <select
-        className="form-select"
-        onChange={(event) => setCategory(event.target.value)}
-      >
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList category={category} />
-    </>
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
   );
 }
 export default App;
