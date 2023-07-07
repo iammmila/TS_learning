e6fe42804cee6826373a06daa1dbb7b24762b4a1
iammmila <ibramila2002@gmail.com>
2023-07-07 16:33:33 +0400

    6.10. Deleting data

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
     //async get ->  await promises -> response / error;
     const controller = new AbortController();
@@ -32,13 +33,35 @@ function App() {
     return () => controller.abort();
   }, []);
 
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+  
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
-      <ul>
+      <ul className="list-group">
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => deleteUser(user)}
+            >
+              delete
+            </button>
+          </li>
         ))}
       </ul>
     </>
